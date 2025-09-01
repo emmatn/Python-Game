951270e00845ed1c7b721320f57c22d19617649a
emmatn <emmathuy2005@gmail.com>
2025-09-01 20:44:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def load_sprite_sheets(dir1, dir2, width, height, direction=False):
         
         for i in range(sprite_sheet.get_width() // width):
             surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
-            # create surface that is size of our desired individual animation frame 
-            # grab image, draw it on surface and export it 
+             # create surface that is size of our desired individual animation frame 
+             # grab image, draw it on surface and export it 
             rect = pygame.Rect(i * width, 0, width, height)
             surface.blit(sprite_sheet, (0,0), rect)
             sprites.append(pygame.transform.scale2x(surface)) # double image size
@@ -58,6 +58,8 @@ class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0) 
     GRAVITY = 1 
     SPRITES = load_sprite_sheets("MainCharacters", "PinkMan", 32, 32, True)
+     # account for delay in changing sprite
+    ANIMATION_DELAY = 3
     
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -84,18 +86,30 @@ class Player(pygame.sprite.Sprite):
             self.direction = "right"
             self.animation_count = 0
     
-    #be called once every frame, move character in right direction and update animation 
-    # increase y velociy by gravity, adjust in terms of how long we have been falling for
+    # be called once every frame, move character in right direction and update animation 
+    # increase y velocity by gravity, adjust in terms of how long we have been falling for
     # picks up pace with every tile 
     
     def loop(self, fps):
-        # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
+         # self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
         
         self.fall_count += 1
+        self.update_sprite()
+    
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+        
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+         # every 5 frames, want to show diff sprite in whatever animation we are using
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
     
     def draw(self, wind):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         wind.blit(self.sprite, (self.rect.x, self.rect.y))
     
         
