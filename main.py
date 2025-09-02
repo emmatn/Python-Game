d8f1b793468cb2d58a148eb16396db13fc2da5b3
emmatn <emmathuy2005@gmail.com>
2025-09-02 20:40:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -79,6 +79,8 @@ class Player(pygame.sprite.Sprite):
         self.animation_count = 0
         self.fall_count = 0 # air time to increment velocity
         self.jump_count = 0
+        self.hit = False
+        self.hit_count = 0 
         
     def jump(self):
         self.y_vel = -self.GRAVITY * 8 # 8 is speed of jump
@@ -90,7 +92,11 @@ class Player(pygame.sprite.Sprite):
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
-    
+        
+    def been_hit(self):
+        self.hit = True
+        self.hit_count = 0
+        
     def move_left(self, vel):
         self.x_vel = -vel
         if self.direction != "left":
@@ -112,6 +118,12 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
         
         self.fall_count += 1
+        if self.hit:
+            self.hit_count += 1
+        if self.hit_count > fps * 2:
+            self.hit = False
+            self.hit_count = 0
+            
         self.update_sprite()
         
     def landed(self):
@@ -125,13 +137,16 @@ class Player(pygame.sprite.Sprite):
     
     def update_sprite(self):
         sprite_sheet = "idle"
-        if self.y_vel != 0:
-            if self.jump_count == 1:
-                sprite_sheet = "jump"
-            elif self.jump_count == 2:
-                sprite_sheet = "double_jump"
-        elif self.y_vel > self.GRAVITY*2: 
-            sprite_sheet = "fall"
+        if self.hit:
+            sprite_sheet = "hit"
+        if self.jump_count > 0:
+            if self.y_vel != 0:
+                if self.jump_count == 1:
+                    sprite_sheet = "jump"
+                elif self.jump_count == 2:
+                    sprite_sheet = "double_jump"
+            elif self.y_vel > self.GRAVITY*2: 
+                sprite_sheet = "fall"
         elif self.x_vel != 0:
             sprite_sheet = "run"
         
@@ -171,7 +186,35 @@ class Block(Objects):
         self.image.blit(block, (0,0))
         self.mask = pygame.mask.from_surface(self.image)
         
-     
+class Fire(Objects):
+    ANIMATION_DELAY = 3
+    
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height, "fire")
+        self.fire = load_sprite_sheets("Traps", "Fire", width, height)
+        self.image = self.fire["off"][0]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.animation_count = 0
+        self.animation_name = "off"
+        
+    def on(self):
+        self.animation_name = "on"
+    def off(self):
+        self.animation_name = "off"
+    
+    def loop(self):
+        sprites = self.fire[self.animation_name]
+         # every 5 frames, want to show diff sprite in whatever animation we are using
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.image = sprites[sprite_index]
+        self.animation_count += 1
+         # update method
+        self.rect = self.image.get_rect(topleft=(self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.image)
+         # since fire static, we need to reset animation count that can lag program 
+        if self.animation_count // self.ANIMATION_DELAY > len(sprites):
+            self.animation_count = 0
+    
  # return list of all background tiles
  # run from directory the file exists in (MAKE SURE CORRECT DIRECTORY)
 def get_background(name):
@@ -197,20 +240,46 @@ def handle_vertical_collision(player, objects, dy):
                 player.rect.top = obj.rect.bottom # set head equal to top of block (COLLIDE NOT THROUGH)
                 player.hit_head()
                 
-        collided_objects.append(obj)
-        
+            collided_objects.append(obj)   
     return collided_objects
+ 
+ # first check that if u hit on L or R it doesnt think its the top 
+def collide(player, objects, dx):
+     # preemitly moving player to where they would be if moving L or R 
+    player.move(dx, 0)
+    player.update()
+    collided_objects = None
+    
+     # checking if collided 
+    for obj in objects: 
+        if pygame.sprite.collide_mask(player, obj):
+            collided_objects = obj
+            break 
+     # reverse movement 
+    player.move(-dx, 0)
+    player.update()
+    return collided_objects
+    
 
 def handle_move(player, objects):
     keys = pygame.key.get_pressed()
     
     player.x_vel = 0
-    if keys[pygame.K_LEFT]:
+    collide_left = collide(player, objects, -PLAYER_VEL*2)
+    collide_right = collide(player, objects, PLAYER_VEL*2
+                            )
+     # check if we are able to move left or right, move if no collision
+    if keys[pygame.K_LEFT] and not collide_left:
         player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and not collide_right:
         player.move_right(PLAYER_VEL)
         
-    handle_vertical_collision(player, objects, player.y_vel)
+    vertical_collide = handle_vertical_collision(player, objects, player.y_vel)
+    to_check = [collide_left, collide_right, *vertical_collide]
+    for obj in to_check: 
+        if obj and obj.name == "fire":
+            player.been_hit()
+    
         
 def draw(window, background, bg_image, player, objects, offset_x):
     for tile in background: 
@@ -231,11 +300,16 @@ def main(window):
     block_size = 96
     player = Player(100, 100, 50, 50)
      # create blocks that go to left and right of screen
+    fire = Fire(100, HEIGHT - block_size - 64, 16, 32)
+    fire.on()
     floor = [Block(i * block_size, HEIGHT - block_size, block_size) 
              for i in range(-WIDTH // block_size, WIDTH*2 // block_size)]
-    
+     # asterisk breaks all elements in floor in objects var
+    objects = [*floor, Block(0, HEIGHT - block_size*2, block_size), 
+               Block(block_size*3, HEIGHT - block_size*4, block_size),
+               fire]
     offset_x = 0
-    scroll_area_width = 200 # start scrolling when 200 pixels to L or R 
+    scroll_area_width = 280 # start scrolling when 200 pixels to L or R 
     
     run = True 
     while run: 
@@ -251,8 +325,9 @@ def main(window):
                     player.jump()
         
         player.loop(FPS) # what moves player
-        handle_move(player, floor)
-        draw(window,background,bg_image, player, floor, offset_x)
+        fire.loop()
+        handle_move(player, objects)
+        draw(window,background,bg_image, player, objects, offset_x)
          
          # explanation? 
         if ((player.rect.right - offset_x >= WIDTH - scroll_area_width and player.x_vel > 0) or (
